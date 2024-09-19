54648fd17f51e5ae1ef07f3606e97dc0ca6f492a
Quentin-Leblanc <artofleb@gmail.com>
2024-09-19 22:44:57 +0200

    Configuration initiale du projet avec Express et dotenv

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Charger les variables d'environnement
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
